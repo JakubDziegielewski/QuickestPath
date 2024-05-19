caf500832d7747f538da5369a55032acbe29341d
kubasliwa <115989174+kubasliwa@users.noreply.github.com>
2024-05-19 17:10:52 +0200

    fix: determine left turns

--- left_turn_handler.py
+++ left_turn_handler.py
@@ -1,5 +1,5 @@
 import networkx as nx
-from graph_utils import get_vector_between_nodes, calculate_sin, calculate_cos, calculate_angle
+from graph_utils import get_vector_between_nodes, calculate_sin, calculate_cos, calculate_angle, compare_highways
 
 
 # klasa ma na celu udostępnienie funkcjonalności rozpoznawania skrętów w lewo w grafie
@@ -15,9 +15,12 @@ class LeftTurnHandler:
 
 
     def is_turn_left(self, G: nx.MultiDiGraph, first_node_id: int, second_node_id: int, third_node_id: int) -> bool:
+        # sprawdzenie, czy badane węzły są połączone krawędziami
+        if (first_node_id, second_node_id) not in nx.edges(G, [first_node_id]) or (second_node_id, third_node_id) not in nx.edges(G, [second_node_id]):
+            raise ValueError("Badanie skrętu dla niepoprawnych danych!")
         
-        # jeżeli środkowy węzeł ma tylko 2 wychodzące krawędzie to wiadomo, że jest to węzeł w środku drogi
-        if len(G[second_node_id]) == 2:
+        # jeżeli środkowy węzeł ma tylko 2 sąsiadujące węzły to wiadomo, że jest to węzeł w środku drogi
+        if len([neighbor for neighbor in nx.all_neighbors(G, second_node_id)]) == 2:
             return False
         
         # wyznacz wektory pomiędzy punktami 
@@ -30,7 +33,7 @@ class LeftTurnHandler:
         
         # wyznacz kąt (w stopniach)
         alpha = calculate_angle(sin_alpha, cos_alpha)
-
+        
         # jeśli obliczony kąt jest większy niż zadana wartość klasyfikująca skręt jako skręt w lewo
         # to zwracamy True, wpp: False
         if alpha > self._min_angle_left_turn:
@@ -39,4 +42,24 @@ class LeftTurnHandler:
         
     
     def calculate_penalty(self, G: nx.MultiDiGraph, first_node_id: int, second_node_id: int, third_node_id: int) -> float:
-        pass
+        # sprawdzenie, czy badane węzły są połączone krawędziami
+        if (first_node_id, second_node_id) not in nx.edges(G, [first_node_id]) or (second_node_id, third_node_id) not in nx.edges(G, [second_node_id]):
+            raise ValueError("Próba naliczenia kary za skręt dla niepoprawnych danych!")
+            
+        # wyznaczenie kategorii obu dróg
+        from_highway = G.get_edge_data(first_node_id, second_node_id, 0)["highway"]
+        to_highway = G.get_edge_data(second_node_id, third_node_id, 0)["highway"]
+        
+        # porównanie kategorii na podstawie zdefiniowanego porządku liniowego kategorii dróg
+        highway_comparison = compare_highways(from_highway, to_highway)
+        
+        # zwrócenie właściwej kary na podstawie uzyskanego wyniku porównania
+        if highway_comparison == -1:
+            return self._penalty_to_worse_road
+        elif highway_comparison == 0:
+            return self._penalty_to_equal_road
+        elif highway_comparison == 1:
+            return self._penalty_to_better_road
+        else:
+            raise ValueError("Niespodziewany błąd przy wyliczaniu kary za skręt w lewo.")
+        
